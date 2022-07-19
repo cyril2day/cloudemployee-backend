7e10aa69562ec61c6cf4e61b2fdd4f9406a54272
psi <cy.lonsido@gmail.com>
2022-07-19 12:38:19 +0800

    update app to use routes

--- api/index.ts
+++ api/index.ts
@@ -1,36 +1,32 @@
 import express from 'express'
 import morgan from 'morgan'
 import mongoose from 'mongoose'
+import routes from '../routes/api'
 
-/*
+
+/**
  * The application instance
  *
  */
+
 const app = express()
 
-/*
- * The application port
- *
- */
-const port = process.env.PORT || 5000
 
 /**
  * Connect to MongoDB. If connected, start
  * server instance.
  *
  */
+
 const dbURI = 'mongodb+srv://cyril:cyriltest@cluster0.z0hpt.mongodb.net/cyriltest?retryWrites=true&w=majority'
 mongoose.connect(dbURI)
-  .then(() => {
-    app.listen(port)
-    console.log(`Application listening on port ${port}`)
-  })
-  .catch(err => console.log(err))
 
-/*
+
+/**
  * For middlewares & static files
  * 
  */
+
 app.use(express.static('public'))
 app.use(express.json())
 app.use(morgan('dev'))
@@ -38,5 +34,12 @@ app.use((request, response, next) => {
   response.locals.path = request.path
   next()
 })
+
+/**
+ * Register routes
+ */
+app.use('/api/', routes)
+
+
 
 export default app
